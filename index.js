4d6b340fd03dc3c61a052718255103f95c528288
Maedinha <gabrieltrevelim@gmail.com>
2023-11-16 10:15:44 +0900

    configurando o crud e criando a entidade Papel

--- index.js
+++ index.js
@@ -12,9 +12,29 @@ app.get('/', (req, res) => {
     res.send('Hello World from Homepage');
 });
 
-app.get('/user', (req, res) => {
-    console.log('TEST');
-    res.send('Hello World from userpage');
-});
+app.get('/papeis/:id', (req, res) => {
+    return res.json(req.params.id);
+})
+
+app.post('/papeis', (req, res) => {
+    const papel = new Papel(req.body.id, req.body.owner, req.body.quantity, req.body.name, req.body.value);
+    return res.json(papelRepository.save(papel));
+})
+
+app.put('/papeis/:id', (req, res) => {
+    if (existsById(req.params.id)) {
+        papelRepository.update(req.body);
+        return res.status(204);
+    }
+    return res.status(422);
+})
+
+app.delete('/papeis/:id', (req, res) => {
+    if (existsById(req.params.id)) {
+        papelRepository.delete(req.body);
+        return res.status(204);
+    }
+    return res.status(422);
+})
 
 app.listen(PORT, () => console.log('Hello World'));
---
+++ papel.js
@@ -0,0 +1,9 @@
+class Papel{
+    constructor(id, owner, quantity, name, value){
+        this.id = id;
+        this.owner = owner;
+        this.quantity = quantity;
+        this.name = name;
+        this.value = value;
+    }
+}
